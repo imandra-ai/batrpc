7f153893d0bf9fcacc156b87483beb1c37e85874
Simon Cruanes <simon.cruanes.2007@m4x.org>
2024-03-13 10:48:01 -0400

    docs

--- src/batrpc.ml
+++ src/batrpc.ml
@@ -1,6 +1,11 @@
 (** Binary RPC over streams of bytes for protobuf services. *)
 
+(** {2 Core library} *)
+
 include Batrpc_core
+
+(** {2 Unix-based networking implementations} *)
+
 module Batrpc_unix = Batrpc_unix
 module Tcp_server = Batrpc_unix.Tcp_server
 module Tcp_client = Batrpc_unix.Tcp_client
